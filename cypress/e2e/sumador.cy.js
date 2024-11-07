eb81750da47128f71220d731498afa34fd947842
Vivian Revollo <revollovivian@gmail.com>
2024-11-07 00:53:14 -0330

    Cypress funcionando

--- cypress/e2e/sumador.cy.js
+++ cypress/e2e/sumador.cy.js
@@ -1,9 +1,31 @@
-describe("Sumador", () => {
-  it("Shows the amount of the addition to the user", () => {
+describe("Calculadora de Cadena", () => {
+  beforeEach(() => {
     cy.visit("/");
-    cy.get("#primer-numero").type(4);
-    cy.get("#segundo-numero").type(5);
-    cy.get("#sumar-button").click();
-    cy.get("#resultado-div").should("contain", "9");
   });
+
+  it("debería mostrar el resultado de la suma para una cadena con un solo número", () => {
+    cy.get("#cadena").type("2");
+    cy.get("#sumar-button").click();
+    cy.get("#resultado-div").should("contain", "2");
+  });
+
+  it("debería sumar números separados por coma", () => {
+    cy.get("#cadena").type("1,2,4,8");
+    cy.get("#sumar-button").click();
+    cy.get("#resultado-div").should("contain", "15");
+  });
+
+  it("debería sumar números separados por comas y guiones", () => {
+    cy.get("#cadena").type("1-2,3");
+    cy.get("#sumar-button").click();
+    cy.get("#resultado-div").should("contain", "6");
+  });
+
+  it("debería ignorar números mayores a 1000", () => {
+    cy.get("#cadena").type("2,1001");
+    cy.get("#sumar-button").click();
+    cy.get("#resultado-div").should("contain", "2");
+  });
+
 });
+
